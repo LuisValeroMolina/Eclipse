3515add63fc2fa039bc0b901feca35f2e9968206
jesus <jesus.everisschool@gmail.com>
2019-05-28 12:18:08 +0200

    Añadidos saludos

--- Prueba/src/everis/eclipse/prueba/Prueba.java
+++ Prueba/src/everis/eclipse/prueba/Prueba.java
@@ -5,6 +5,8 @@ public class Prueba {
 	public static void main(String[] args) {
 
 		System.out.println("¡Hola mundo!");
+		System.out.println("¡Hola mundo!");
+		System.out.println("¡Hola mundo!");
 
 	}
 
